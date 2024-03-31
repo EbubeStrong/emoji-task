1c7e017dbef4faa4fad4224daa588dc9902fba3a
kenechi <nsudekenechi2@gmail.com>
2024-03-31 21:41:26 +0100

    Finished React

--- emoji-project/src/Components/EmojiCard.jsx
+++ emoji-project/src/Components/EmojiCard.jsx
@@ -8,87 +8,52 @@ import '../Styles/EmojiCard.css'
 
 const EmojiCard = () => {
 
-  let data = []
-  const [emojiData, setEmojiData] = useState([])
-  const [input, setinput] = useState('')
+  const [emojiData, setEmojiData] = useState({
+    data: [],
+    filteredData: []
+  })
   useEffect(() => {
-      fetch("https://emoji-api.com/emojis?access_key=dc9be100e84f19a4eb9454e8542b2616dbca7c9c") 
+    fetch("https://emoji-api.com/emojis?access_key=dc9be100e84f19a4eb9454e8542b2616dbca7c9c")
       .then(response => response.json())
       .then(emojiData => {
-        // setEmojiData(emojiData)
-          // console.log(emojiData);
-          data = emojiData; 
-  // 
-          // if(emojiInput.value !== ''){
-          //     // data.forEach(emoji => {
-          //     //     const span = document.createElement('span')
-          //     //     span.setAttribute('emoji-name', emoji.slug)
-          //     //     span.textContent = emoji.character
-          //     //     emojisContainer.appendChild(span)
-          //     // });
-          //     // Append emojisContainer to card outside the loop
-              
-          // // card.appendChild(emojisContainer);
-          // }
+        setEmojiData(item => ({ ...item, data: emojiData }))
       })
       .catch(error => {
-          console.log('Error fetching emojis:', error);
+        console.log('Error fetching emojis:', error);
       });
-    }, [])
+  }, [])
 
-    const filterEmoji = () => {
+  const filterEmoji = (search) => {
+    let filteredData = search == "" ? [] : emojiData.data.filter(item => item.slug.toLowerCase().includes(search.toLowerCase()));
+    setEmojiData(prev => ({ ...prev, filteredData }))
+  }
 
-    }
-
-// emojiForm.addEventListener('submit', e => {
-//     e.preventDefault(); // Prevent the default form submission behavior
-    
-//     const searchTerm = emojiInput.value.trim(); // Trim to remove leading and trailing spaces
-    
-//     if (searchTerm) {
-//         // Clear previous emoji elements
-//         emojisContainer.innerHTML = '';
-
-//         // Filter and append emojis that match the search term
-//         data.forEach(emoji => {
-//             if (emoji.slug.toLowerCase().includes(searchTerm.toLowerCase())) {
-//                 const span = document.createElement('span')
-//                 span.setAttribute('emoji-name', emoji.slug)
-//                 span.textContent = emoji.character
-//                 emojisContainer.appendChild(span)
-//             }
-//         });
-//     } else {
-//         // If input is empty, hide the emojisContainer
-//         emojisContainer.style.display = 'none';
-//     }
-// });
 
   return (
     <>
-    <form className="emojiForm">
-    <div className="inputForm">
-      <input type="text" className="emojiInput" placeholder="Search Emoji" />
-      <span class="loader">⏳</span>
-    </div>
+      <form className="emojiForm">
+        <div className="inputForm">
+          <input type="text" className="emojiInput" placeholder="Search Emoji" onKeyUp={(e) => filterEmoji(e.target.value)} />
+          <span className="loader">⏳</span>
+        </div>
 
-    <div className="btn">
-      <button type="submit">
-        🔍
-      </button>
-    </div>
-  </form>
+        <div className="btn">
+          <button type="submit">
+            🔍
+          </button>
+        </div>
+      </form>
 
-  {input && <div className="card">
-    <div id="emojiList">
-      {emojiData.map(item => <span emoji-name = {item.slug}>
-        {item.character}
-      </span>)}
-    </div>
-  </div>}
+      <div className="card">
+        <div id="emojiList">
+          {emojiData.filteredData.map((item, index) => <span emoji-name={item.slug} key={index}>
+            {item.character}
+          </span>)}
+        </div>
+      </div>
 
-  
-  </>
+
+    </>
   )
 }
 
